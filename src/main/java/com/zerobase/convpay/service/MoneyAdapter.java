b25e7fb4ed52bf3d57d2a94f135acd502ae0afb4
itonse <govl3dnjs@gmail.com>
2023-04-14 11:09:32 +0900

    커밋1

--- src/main/java/com/zerobase/convpay/service/MoneyAdapter.java
+++ src/main/java/com/zerobase/convpay/service/MoneyAdapter.java
@@ -2,5 +2,7 @@ package com.zerobase.convpay.service;
 
 public class MoneyAdapter {   // 결제 서비스 -> 머니 어댑터
 
-
+    public static void main(String[] args) {
+        System.out.println("** 커밋 완료");
+    }
 }
