2b7a49ba25f0785c6630ab5ad20213d44f9dae34
itonse <govl3dnjs@gmail.com>
2023-04-14 11:04:37 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/convpay/service/ConveniencePayService.java
@@ -0,0 +1,13 @@
+package com.zerobase.convpay.service;
+
+public class ConveniencePayService {   // 결제 서비스(편결이에서 가장 메인이 되는 서비스)
+
+    public void pay() {   // 결제 기능
+
+    }
+
+    public void payCancel() {  // 결제취소 기능
+
+    }
+
+}
---
+++ src/main/java/com/zerobase/convpay/service/MoneyAdapter.java
@@ -0,0 +1,6 @@
+package com.zerobase.convpay.service;
+
+public class MoneyAdapter {   // 결제 서비스 -> 머니 어댑터
+
+
+}
