59d83b01dadd9fd6b7877b6b44e91e13d1e20892
itonse <govl3dnjs@gmail.com>
2023-04-19 12:17:45 +0900

    스프링으로 전환하기.

--- src/main/java/com/zerobase/convpay/UserClient.java
+++ src/main/java/com/zerobase/convpay/UserClient.java
@@ -8,12 +8,20 @@ import com.zerobase.convpay.dto.PayResponse;
 import com.zerobase.convpay.service.ConveniencePayService;
 import com.zerobase.convpay.type.ConvenienceType;
 import com.zerobase.convpay.type.PayMethodType;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class UserClient {     // * UML: '[사용자(클라이언트)]' -> [결제 서비스(편결이)] -> [머니어댑터] *   에서 '사용자 '(외부)
     public static void main(String[] args) {
-        // 사용자는 Applicationconfig만 이용.
-        Applicationconfig applicationconfig = new Applicationconfig();
-        ConveniencePayService conveniencePayService = applicationconfig.conveniencePayServiceDiscountPayMethod();
+        // 사용자는 스프링 안으로 접근
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(Applicationconfig.class);
+            // 스프링 컨테이너를 관리하는 applicationContext를 만듦. 만들면서 구성 정보는 Applicationconfig.class 를 참고하도록 요청.
+
+        ConveniencePayService conveniencePayService =
+                applicationContext.getBean("conveniencePayService",
+                        ConveniencePayService.class);
+            // ConveniencePayService 에서 등록했던 Bean들을 가져옴.
 
         // G25, 결제 50원
         PayRequest payRequest = new PayRequest(PayMethodType.CARD,
